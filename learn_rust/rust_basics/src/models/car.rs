4db26d0d8e23256142c4cf078f514968560aca28
Kelium Jojoa Urrutia <keliumju@gmail.com>
2024-12-02 19:24:59 -0500

    Move factory card to card mod

--- learn_rust/rust_basics/src/models/car.rs
+++ learn_rust/rust_basics/src/models/car.rs
@@ -21,3 +21,19 @@ pub fn car_factory(color: String, transmission: Transmission, convertible: bool)
         mileage: 0
     }
 }
+
+pub fn car_main_ex() {
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", 
+        car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Silver"), Transmission::Automatic, false);
+
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Gold"), Transmission::SemiAuto, true);
+
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.color, car.transmission, car.convertible, car.mileage);
+}
